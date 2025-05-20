39d4cfc08174d827a91595cd80bd2b1d95fb58f2
Daniel Marques <daniel.brz2009@gmail.com>
2025-05-20 11:53:44 -0300

    Refactor AWS environment variables for S3 integration
    - Renamed AWS environment variables in env.js to reflect S3 configuration.
    - Updated variable names from AWS_REGION, AWS_ACCESS_KEY_ID, and AWS_SECRET_ACCESS_KEY to AWS_S3_REGION, AWS_S3_ACCESS_KEY_ID, and AWS_S3_SECRET_ACCESS_KEY for consistency with S3 usage.

--- src/env.js
+++ src/env.js
@@ -8,9 +8,9 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
 
-    AWS_REGION: z.string(),
-    AWS_ACCESS_KEY_ID: z.string(),
-    AWS_SECRET_ACCESS_KEY: z.string(),
+    AWS_S3_REGION: z.string(),
+    AWS_S3_ACCESS_KEY_ID: z.string(),
+    AWS_S3_SECRET_ACCESS_KEY: z.string(),
     AWS_S3_BUCKET_NAME: z.string(),
   },
 
@@ -27,9 +27,9 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    AWS_REGION: process.env.AWS_REGION,
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_S3_REGION: process.env.AWS_S3_REGION,
+    AWS_S3_ACCESS_KEY_ID: process.env.AWS_S3_ACCESS_KEY_ID,
+    AWS_S3_SECRET_ACCESS_KEY: process.env.AWS_S3_SECRET_ACCESS_KEY,
     AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
