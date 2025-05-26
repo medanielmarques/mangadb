3ff58e3242c0f84e3cfea243aef6279e6efc24bd
Daniel Marques <daniel.brz2009@gmail.com>
2025-05-26 10:15:34 -0300

    Refactor getBaseUrl function for improved URL handling
    - Enhanced the getBaseUrl function to dynamically determine the base URL from environment variables, ensuring proper formatting for production and development environments.
    - Added checks for both NEXT_PUBLIC_BASE_URL and NEXT_PUBLIC_VERCEL_URL, with a fallback to localhost, improving deployment flexibility.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -7,7 +7,15 @@ export const supabase = createClient(
 )
 
 function getBaseUrl() {
-  return env.NEXT_PUBLIC_BASE_URL
+  let url =
+    process?.env?.NEXT_PUBLIC_BASE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3001"
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith("http") ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`
+  return url
 }
 
 export async function signInWithDiscord() {
