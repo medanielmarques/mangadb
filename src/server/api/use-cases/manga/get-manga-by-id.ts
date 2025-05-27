5f873c0542138eaea463010a9c486454d2de4ae4
Daniel Marques <daniel.brz2009@gmail.com>
2025-05-27 09:48:52 -0300

    Enhance getMangaById use case to include detailed manga information and cover art URL retrieval
    - Updated the getMangaById use case to fetch additional details such as authors, artists, status, volume and chapter counts, and average rating.
    - Implemented inner joins with related tables (volumes, images, reviews) to improve data integrity and retrieval.
    - Integrated a storage service to obtain signed URLs for cover art, enhancing the response structure for better client-side usage.

--- src/server/api/use-cases/manga/get-manga-by-id.ts
+++ src/server/api/use-cases/manga/get-manga-by-id.ts
@@ -1,12 +1,34 @@
+import { storageService } from "@/server/api/services/storage"
 import { db } from "@/server/db"
-import { mangas } from "@/server/db/schema"
+import { chapters, images, mangas, reviews, volumes } from "@/server/db/schema"
 import { TRPCError } from "@trpc/server"
-import { eq } from "drizzle-orm"
+import { and, avg, count, eq } from "drizzle-orm"
 
 export async function getMangaByIdUseCase({ id }: { id: string }) {
-  const manga = await db.query.mangas.findFirst({
-    where: eq(mangas.id, id),
-  })
+  const manga = await db
+    .select({
+      id: mangas.id,
+      title: mangas.title,
+      description: mangas.description,
+      coverArtUrl: images.url,
+      authors: mangas.authors,
+      artists: mangas.artists,
+      status: mangas.status,
+      publishedAt: mangas.publishedAt,
+      volumes: count(volumes.id),
+      chapters: count(chapters.id),
+      avgRating: avg(reviews.rating),
+    })
+    .from(mangas)
+    .where(eq(mangas.id, id))
+    .innerJoin(volumes, eq(mangas.id, volumes.mangaId))
+    .innerJoin(images, and(eq(images.entityId, volumes.id)))
+    .innerJoin(reviews, eq(mangas.id, reviews.mangaId))
+    .groupBy(mangas.id, mangas.title, images.url)
+
+  const coverArtUrl = await storageService.getSignedUrl(
+    manga[0]?.coverArtUrl ?? "",
+  )
 
   if (!manga) {
     throw new TRPCError({
@@ -15,5 +37,8 @@ export async function getMangaByIdUseCase({ id }: { id: string }) {
     })
   }
 
-  return manga
+  return {
+    ...manga[0],
+    coverArtUrl,
+  }
 }
